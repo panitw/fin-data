33e8fb6374055745c0556a9d36a2517ac8a61929
Panit Wechasil <panitw@hotmai.com>
2016-07-27 03:17:54 +0700

    Fix linting config

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var fin = require('./index');
 
 
 var df = new fin.DataFrame({
-    'c1': [0, 1, 2, 3],
-    'c2': [4, 5, 6, 7]
+    'c1' : [0, 1, 2, 3],
+    'c2' : [4, 5, 6, 7]
 });
 
 console.log(df.index());
